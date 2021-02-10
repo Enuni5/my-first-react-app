0e620e3c1c310ad658f80a65b5c1b784d7f20501
Enuni5 <emilio.nuni@gmail.com>
2021-02-10 18:30:35 +0100

    Little lifecycle practice

---
+++ src/Counter.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    console.log('Constructor');
+    super(props);
+
+    this.state = {
+      counter: 0,
+      seed: 0,
+    };
+
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed,
+      };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+    console.log('--------------------');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log('Should Component Update - DO NOT RENDER');
+      console.log('--------------------');
+
+      return false;
+    }
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Get Snapshot Before Update');
+    return null;
+  }
+
+  render() {
+    console.log('Render', this.state.error);
+    if (this.state.error) {
+      return <div>We have encountered an error! {this.state.error}</div>;
+    }
+
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <div className='counter'>Counter: {this.state.counter}</div>
+        <ErrorComponent />
+      </div>
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Component Did Update');
+    console.log('-------------------');
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount');
+    console.log('--------------');
+  }
+
+  componentDidCatch(error, info) {
+    console.log('Component Did Catch');
+    this.setState({ error, info });
+  }
+}
